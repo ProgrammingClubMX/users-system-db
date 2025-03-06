3d8e3a663852a3ce024174c1efb2e8981f54cc2a
ProgrammingClubMX <programming.club.mx@gmail.com>
2025-03-06 03:19:29 +0000

    agregue la tabla profile y la conecte al usuario

---
+++ DDL-2025-03-05.sql
@@ -0,0 +1,18 @@
+CREATE TABLE profile(
+    id              UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    fullName        TEXT NOT NULL,
+    phoneNumber     VARCHAR(50)
+);
+
+--corregir el nombre de las columnas
+ALTER TABLE profile RENAME COLUMN "fullname" TO "fullName";
+ALTER TABLE profile RENAME COLUMN "phonenumber" TO "phoneNumber";
+
+ALTER TABLE "profile"
+    ADD COLUMN "userId" UUID NOT NULL DEFAULT uuid_generate_v4();
+
+ALTER TABLE "profile"
+    ADD CONSTRAINT profile_to_user
+        FOREIGN KEY ("userId")
+        REFERENCES "user" ("id");
+        
