c0dc67e0b3f77e8addbfce50e2adec68a71f08ab
Karla Bravo <karlabravoc.qi@gmail.com>
2025-03-13 21:30:22 -0600

    Agregue tablas para el status del usurio

---
+++ DDL-2025-03-13-02.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "userStatus" (
+  "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "label" VARCHAR (50),
+  "editable" BOOLEAN DEFAULT (true)
+);
+
+CREATE TABLE "userToStatus" (
+  "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4 (),
+   "userId" UUID NOT NULL,
+   "statusId" UUID NOT NULL 
+);
+
+ALTER TABLE "userToStatus"
+  ADD CONSTRAINT "userToStatus_to_userStatus"
+  FOREIGN KEY ("statusId")
+  REFERENCES "userStatus" ("id");
+
+ALTER TABLE "userToStatus"
+  ADD CONSTRAINT "userToStatus_to_user"
+  FOREIGN KEY ("userId")
+  REFERENCES "user" ("id");
