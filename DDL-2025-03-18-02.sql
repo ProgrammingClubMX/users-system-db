b9d130ac818cfabdbc2568fb7262c9cb81941de9
Karla Bravo <karlabravoc.qi@gmail.com>
2025-03-18 20:43:21 -0600

    Se conecto tablas de grupo a usuario

--- DDL-2025-03-18-02.sql
+++ DDL-2025-03-18-02.sql
@@ -12,4 +12,13 @@ CREATE TABLE "userToGroup"(
     "groupId" UUID NOT NULL
 );
 
+ALTER TABLE "userToGroup"
+    ADD CONSTRAINT "userToGroup_to_user"
+    FOREIGN kEY ("userId")
+    REFERENCES "user"("id");
 
+ALTER TABLE "userToGroup" -- altera la tabla "userToGroup"
+    ADD CONSTRAINT "userToGroup_to_group" -- si la tabla existe, agrega una nueva restriccion que se va a llamar "userToGroup_to_group"
+    FOREIGN KEY ("groupId") -- si no existe una restricción con el mismo nombre, convierte la columna "groupId" en una llave foránea
+    REFERENCES "group"("id"); -- si lograste convertir la columna "groupId" en llave foránea, has referencia a la columna "id" de la tabla "group"
+ 
