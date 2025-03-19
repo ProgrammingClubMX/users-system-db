66a29ac587121c48f7b4565e4ccd2a1c9ba83bc2
Karla Bravo <karlabravoc.qi@gmail.com>
2025-03-18 21:09:10 -0600

    Agrego comentarios sobre como explicar en lenguaje natural

--- DDL-2025-03-18-02.sql
+++ DDL-2025-03-18-02.sql
@@ -1,15 +1,15 @@
 -- Se crearón tablas de grupos para el usuario
-CREATE TABLE "group"(
-    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(350),
-    "description" TEXT,
-    "urlImage" TEXT
+CREATE TABLE "group"( -- crear tabla con el nombre "group"
+    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), -- se crea una tabla llamada "id" del tipo de dato UUID que debe asegurar que no esta nulo y tendra una clave primaria y en automatigo tendra un valor generado por la funcion UUID
+    "name" VARCHAR(350), --se agregara una columna con el nombre "name" con un tipo de dato VARCHAR (con un maxiomo de 350 caracteres)
+    "description" TEXT, --se agregara una columna con el nombre "description" con un tipo de dato TEXT
+    "urlImage" TEXT --se agregara una columna con el nombre "urlImage" con un tipo de dato TEXT
 );
-
-CREATE TABLE "userToGroup"(
+-- esta es una tabla pivote entre el usuario grupo
+CREATE TABLE "userToGroup"( --se crea una tabla con el nombre "userToGroup"
     "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "userId" UUID NOT NULL,
-    "groupId" UUID NOT NULL
+    "userId" UUID NOT NULL, --se crea la columna "userId" con tipo de dato UUID con una restricción no nula (para conectar la tabla "user")
+    "groupId" UUID NOT NULL --se crea la columna "groupId" con tipo de dato UUID con una restrcción no nula (para conectar la tabla "group") 
 );
 
 ALTER TABLE "userToGroup"
