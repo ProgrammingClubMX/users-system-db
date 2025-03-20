4271bf36d654168a6954d44e0371631fae579a4a
Karla Bravo <karlabravoc.qi@gmail.com>
2025-03-19 20:44:08 -0600

    Se crean tablas group tags , en comentarios se redacta en lenguaje natural

---
+++ DDL-2025-03-199-02.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "groupTags"( --se crea una tabla llamada ""groupTags"
+    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), --se crea una columna llamada "id" con el tipo de dato UUID con restricion NOT NULL  que sera la llave primaria de la tabla "groupTag"
+    "tag" VARCHAR(50) --se crea columna "tag" con el tipo de dato VARCHAR con 50 caracteres
+);
+
+CREATE TABLE "groupToTags"( --se crea una tabla llamada "groupToTags"
+    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),--se crea una columna llamada "id" con el tipo de dato UUID con restriccion NOT NULL que sera la llave primaria de la tabla "grouptoTag"
+    "groupId" UUID NOT NULL, --se crea columna "groupId" del tipo de dato UUID con restricción NOT NULL
+    "groupTagId" UUID NOT NULL --se crea columna "groupTagId" del tipo de dato UUID con restricción NOT NULL
+); 
