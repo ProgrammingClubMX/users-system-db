14e51bb88e87f0c8d1f5c1667a466a2b020411e7
Karla Bravo <karlabravoc.qi@gmail.com>
2025-03-19 20:03:03 -0600

    se corrige comentarios

--- DDL-2025-03-19-01.sql
+++ DDL-2025-03-19-01.sql
@@ -5,7 +5,7 @@ CREATE TABLE "groupStatus"( --se crea tabla con el nombre "groupStatus"
 );
 
 CREATE TABLE "groupToStatus"( --se crea tabla con el nombre "groupToStatus"
-    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), --se crea una tabla "id" del tipo uuid con restriccion not null que sera la llave primaria
+    "id" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), --se crea una columna "id" del tipo uuid con restriccion not null que sera la llave primaria
     "groupId" UUID NOT NULL, --se crea la columna "groupId" del tipo UUID con restriccion not null
     "groupStatusId" UUID NOT NULL --se crea columna "groupStatusId" del tipo UUID con restriccion not null
 );
